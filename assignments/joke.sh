b797def2809c7865988cd0e50c6c3d7c683f1466
Isabelle Ki <hello@isabelleki.com>
2023-05-03 15:22:24 -0400

    Create joke.sh
    added joke, yay!

---
+++ assignments/joke.sh
@@ -0,0 +1,4 @@
+#This script downloads and prints a joke from the public site "https://icanhazdadjoke.com"
+#To run this file, type ./joke.sh from this folder. 
+echo "My new thesaurus is terrible. In fact, it's so bad, I'd say it's terrible."
+exit 0
